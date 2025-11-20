7c84d61b5d3af9914f5ec3d421dc2d77c988c0fb
ungratefulSnail <dragosbirsan92@gmail.com>
2025-11-20 21:50:14 +0200

    created the JS file and added function getComputerChoice who will randomly return one of these string values: 'Rock', 'Paper' or 'Scissors'

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+/* Math.random() returns values between 0 and 1 - I divided that in 3 parts and made the conditional statement*/
+function getComputerChoice() {
+  const computerChoice = Math.random(); 
+  if (computerChoice > 0.7) {
+    return "Scissors";
+  } else if (computerChoice > 0.3) {
+    return "Paper";
+  } else {
+    return "Rock";
+  }
+};
+
+alert(getComputerChoice())
+
+
