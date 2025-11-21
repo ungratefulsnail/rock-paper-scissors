2200f4e6c18b2d4c53b0058a3e636b1b5e3265ed
ungratefulSnail <dragosbirsan92@gmail.com>
2025-11-21 17:24:33 +0200

    add Score variables and the function playRound for the gameplay of a single round, here needs to be revised

--- javascript.js
+++ javascript.js
@@ -1,19 +1,51 @@
 
 /* --- Computer input --- */
 function getComputerChoice() {
-  const computerChoice = Math.random(); // Math.random() returns values between 0 and 1
-  if (computerChoice > 0.7) { // divide in 3 parts and made the conditional statements
-    return "Scissors";
-  } else if (computerChoice > 0.3) {
-    return "Paper";
+  const choice = Math.random(); // Math.random() returns values between 0 and 1
+  if (choice > 0.7) { // divide in 3 parts and made the conditional statements
+    return "scissors";
+  } else if (choice > 0.3) {
+    return "paper";
   } else {
-    return "Rock";
+    return "rock";
   }
 };
 
 /* --- User input --- */
 function getHumanChoice () {
-  const humanChoice = prompt("Choose between Rock - Paper - Scissors:"); // User enters the value
-  return humanChoice;
+  const choice = prompt("Choose between rock - paper - scissors:"); // User enters the value
+  return choice;
 };
 
+/* --- Score variables --- */
+let humanScore = 0; // set to 0 initially
+let computerScore = 0;
+
+let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
+
+const humanChoiceIns = humanChoice.toLowerCase(); // made humanChoice case-insensitive
+
+/* --- Gameplay of a single round --- */
+function playRound(humanChoiceIns, computerChoice) {
+  
+  if (humanChoiceIns === "rock" && computerChoice === "scissors") {
+    return "You win! rock beats scissors";
+  } else if (humanChoiceIns === "rock" && computerChoice === "paper") {
+    return "You lose! Paper beats Rock";
+  } else if (humanChoiceIns === "paper" && computerChoice === "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (humanChoiceIns === "paper" && computerChoice === "scissors") {
+    return "You lose! Scissors beats Paper";
+  } else if (humanChoiceIns === "scissors" && computerChoice === "paper") {
+    return "You Win! Scissors beats Paper";
+  } else if (humanChoiceIns === "scissors" && computerChoice === "rock") {
+    return "You lose! Rock beats Scissors";
+  } else {
+    return "Tie";
+  }
+}
+
+console.log("User says " + humanChoiceIns)
+console.log("Computer says " + computerChoice);
+console.log(playRound());
