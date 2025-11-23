db8bc90f94834f555e82ee897d6a7cadcfb9ae02
ungratefulSnail <dragosbirsan92@gmail.com>
2025-11-23 22:05:13 +0200

    changed function playRound from declaration to expression after struggling with it, I was getting undefined and always a tie.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 
 /* --- Computer input --- */
+
 function getComputerChoice() {
-  const choice = Math.random(); // Math.random() returns values between 0 and 1
+  let choice = Math.random(); // Math.random() returns values between 0 and 1
   if (choice > 0.7) { // divide in 3 parts and made the conditional statements
     return "scissors";
   } else if (choice > 0.3) {
@@ -12,12 +13,14 @@ function getComputerChoice() {
 };
 
 /* --- User input --- */
+
 function getHumanChoice () {
-  const choice = prompt("Choose between rock - paper - scissors:"); // User enters the value
+  let choice = prompt("Choose between rock - paper - scissors:"); // User enters the value
   return choice;
 };
 
 /* --- Score variables --- */
+
 let humanScore = 0; // set to 0 initially
 let computerScore = 0;
 
@@ -27,23 +30,37 @@ let humanChoice = getHumanChoice();
 const humanChoiceIns = humanChoice.toLowerCase(); // made humanChoice case-insensitive
 
 /* --- Gameplay of a single round --- */
-function playRound(humanChoiceIns, computerChoice) {
+
+let playRound; // Here I used function expression after trying one day to make it work with function declaration.
   
   if (humanChoiceIns === "rock" && computerChoice === "scissors") {
-    return "You win! rock beats scissors";
+    playRound = function() {
+      return "You win! rock beats scissors";
+    }
   } else if (humanChoiceIns === "rock" && computerChoice === "paper") {
-    return "You lose! Paper beats Rock";
+    playRound = function() {
+      return "You lose! Paper beats Rock";
+    }
   } else if (humanChoiceIns === "paper" && computerChoice === "rock") {
-    return "You Win! Paper beats Rock";
+    playRound = function() {
+      return "You Win! Paper beats Rock";
+     }
   } else if (humanChoiceIns === "paper" && computerChoice === "scissors") {
-    return "You lose! Scissors beats Paper";
+    playRound = function() {
+      return "You lose! Scissors beats Paper";
+     }
   } else if (humanChoiceIns === "scissors" && computerChoice === "paper") {
-    return "You Win! Scissors beats Paper";
+    playRound = function() {
+      return "You Win! Scissors beats Paper";
+     }
   } else if (humanChoiceIns === "scissors" && computerChoice === "rock") {
-    return "You lose! Rock beats Scissors";
+    playRound = function() {
+      return "You lose! Rock beats Scissors";
+     }
   } else {
-    return "Tie";
-  }
+    playRound = function() {
+      return "Tie";
+     }
 }
 
 console.log("User says " + humanChoiceIns)
