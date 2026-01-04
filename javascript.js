a5f1d8b26bac0fc6c87dc3173471b08772370044
ungratefulSnail <dragosbirsan92@gmail.com>
2026-01-04 13:09:16 +0200

    Add the selectors for buttons, the event listeners for buttons and the result text in a div

--- javascript.js
+++ javascript.js
@@ -1,11 +1,47 @@
 const CHOICES = ['rock', 'paper', 'scissors'];
 
+const rockBtn = document.querySelector('#rockBtn');
+const paperBtn = document.querySelector('#paperBtn');
+const scissorsBtn = document.querySelector('#scissorsBtn')
+const container = document.querySelector('.container');
+
+const result = document.createElement('div');
+result.className = 'result'
+container.appendChild(result);
+
 /* --- Computer input --- */
 
 // Returns a random choice from the CHOICES array
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * CHOICES.length);
   return CHOICES[randomChoice];
+}
+
+/* Display round result */
+
+// The user input comes from buttons and shows the result in a new created div
+rockBtn.addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    const roundResult = playRound('rock', computerChoice);
+    result.textContent = 'Computer says ' + computerChoice + ': ' + getResultText(roundResult);
+})
+
+paperBtn.addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    const roundResult = playRound('paper', computerChoice);
+    result.textContent = 'Computer says ' + computerChoice + ': ' + getResultText(roundResult);
+})
+
+scissorsBtn.addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    const roundResult = playRound('scissors', computerChoice);
+    result.textContent = 'Computer says ' + computerChoice + ': ' + getResultText(roundResult);
+})
+
+function getResultText(result) {
+  if (result === 'win') return 'You win!';
+  if (result === 'lose') return 'You lose!';
+  return 'Tie!';
 }
 
 /* --- Gameplay of a single round --- */
