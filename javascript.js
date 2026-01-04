afef53565588f3c9161be1a211a31113879243da
ungratefulSnail <dragosbirsan92@gmail.com>
2026-01-04 13:03:28 +0200

    Delete the old logic and simplify the gameplay for a single round

--- javascript.js
+++ javascript.js
@@ -8,24 +8,12 @@ function getComputerChoice() {
   return CHOICES[randomChoice];
 }
 
-/* --- User input --- */
-
-function getHumanChoice () {
-  let choice = prompt("Choose between rock - paper - scissors:");
-  choice = choice.toLowerCase(); // made prompt case-insensitive
-  return choice;
-};
-
-/* --- Score variables --- */ 
-
-let humanScore = 0; // set to 0 initially
-let computerScore = 0;
-
 /* --- Gameplay of a single round --- */
 
 function playRound(humanChoice, computerChoice) {
     
     if (humanChoice === computerChoice) {
+        console.log("Tie!");
         return "tie";
     }
 
@@ -34,35 +22,10 @@ function playRound(humanChoice, computerChoice) {
         (humanChoice === 'scissors' && computerChoice === 'paper') ||
         (humanChoice === 'paper' && computerChoice === 'rock') 
     ) {
+            console.log("You win!");
             return "win";
     } else {
+        console.log("You lose!");
         return "lose";
     }
-}
-
-/* --- Gameplay --- */
-
-function playGame() {
-  
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
-  console.log("User says --" + humanSelection + "-- and Computer says --" + computerSelection + "--");
-  const result = playRound(humanSelection, computerSelection);
-  console.log(result);
-  console.log("User Score: " + humanScore);
-  console.log("Computer Score: " + computerScore);
-}
-
-playGame();
-playGame();
-playGame();
-playGame();
-playGame();
-
-if (humanScore > computerScore) {
-  console.log("User Wins!");
-} else if (humanScore < computerScore) {
-  console.log("Computer Wins!");
-} else {
-  console.log("It's a tie!");
 }
